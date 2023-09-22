96a607c208434753d586d3762fc069e484abf3ca
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-09-22 14:20:31 +0200

    Simple Spring Boot REST API without Security

---
+++ src/main/java/com/dgmf/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.dgmf.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    //Endpoint: http://localhost:8080/welcome
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome to Spring Application without Security";
+    }
+}
