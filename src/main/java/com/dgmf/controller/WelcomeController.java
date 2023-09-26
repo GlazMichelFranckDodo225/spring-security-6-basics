61c35e44a6ce40bc05751af969cbc053d398aa78
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-09-26 11:28:54 +0200

    Securing Spring Boot basic app using Spring Security

--- src/main/java/com/dgmf/controller/WelcomeController.java
+++ src/main/java/com/dgmf/controller/WelcomeController.java
@@ -8,6 +8,6 @@ public class WelcomeController {
     //Endpoint: http://localhost:8080/welcome
     @GetMapping("/welcome")
     public String sayWelcome() {
-        return "Welcome to Spring Application without Security";
+        return "Welcome to Spring Application with Spring Security";
     }
 }
